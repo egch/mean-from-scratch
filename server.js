8dfd9241029b539c249c7d9896d3712167cc42f5
egch <enricogiurin@gmail.com>
2016-04-03 18:15:05 +0200

    step-3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var Person = require('./models/Person.js');
 var app = express();
 
@@ -11,10 +12,25 @@ db.once('open', function() {
   console.log("we are connected");
 });
 
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', function(request, response){
 	Person.find(function(error, data)
 	{
 		response.json(data);
+	});
+});
+
+
+app.post('/', function(request, response){
+	var np = new Person();
+	np.firstName = request.body.firstName;
+	np.lastName = request.body.lastName;
+	np.save(function(error){
+		response.json("A new person has been created");
 	});
 });
 
