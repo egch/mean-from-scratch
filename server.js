6fb07c80708ed025157be136bf029e7e1b190e4b
egch <enricogiurin@gmail.com>
2016-04-03 18:10:34 +0200

    step-2

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var Person = require('./models/Person.js');
 var app = express();
 
+mongoose.connect('mongodb://localhost/mongodbdemo');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we are connected");
+});
+
 app.get('/', function(request, response){
-	response.send('Hello World');
+	Person.find(function(error, data)
+	{
+		response.json(data);
+	});
 });
 
 app.listen(3000, function(){
